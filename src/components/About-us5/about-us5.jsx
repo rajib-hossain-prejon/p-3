83d97ea9cd4332b8f5ff6cb70791f09aa633611b
unknown <you@example.c>
2023-02-12 04:43:04 +0600

    'first'

---
+++ src/components/About-us5/about-us5.jsx
@@ -0,0 +1,68 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+import aboutSkillsProgress from '../../common/aboutSkillsProgress';
+
+const AboutUs5 = () => {
+  React.useEffect(() => {
+    aboutSkillsProgress(
+      document.querySelector('.about-cr .skills-box'),
+      document.querySelectorAll('.skill-progress .progres'),
+      document.querySelector('.about-cr')
+    );
+  }, []);
+  return (
+    <section className='about-cr' id='skills'>
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='col-lg-6 img md-mb50'>
+            <img src='/img/prejon700.jpg' alt='' />
+          </div>
+          <div className='col-lg-5 valign'>
+            <div className='cont full-width'>
+              <h3 className='color-font'>My Skills</h3>
+              <h5 className='co-tit mb-15'>The Skills I use to build Apps</h5>
+              {/* <p>
+                We are Vie. We create award-winning websites, remarkable brands
+                and cutting-edge apps.Nullam imperdie.
+              </p> */}
+              <div className='skills-box mt-40'>
+                <div className='skill-item'>
+                  <h5 className='fz-14 mb-15'>Next.js</h5>
+                  <div className='skill-progress'>
+                    <div className='progres' data-value='70%'></div>
+                  </div>
+                </div>
+                <div className='skill-item'>
+                  <h5 className='fz-14 mb-15'>React</h5>
+                  <div className='skill-progress'>
+                    <div className='progres' data-value='90%'></div>
+                  </div>
+                </div>
+                <div className='skill-item'>
+                  <h5 className='fz-14 mb-15'>React Native</h5>
+                  <div className='skill-progress'>
+                    <div className='progres' data-value='70%'></div>
+                  </div>
+                </div>
+                <div className='skill-item'>
+                  <h5 className='fz-14 mb-15'>Django</h5>
+                  <div className='skill-progress'>
+                    <div className='progres' data-value='80%'></div>
+                  </div>
+                </div>
+                <div className='skill-item'>
+                  <h5 className='fz-14 mb-15'>Redux</h5>
+                  <div className='skill-progress'>
+                    <div className='progres' data-value='80%'></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutUs5;
---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,143 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+import Link from 'next/link';
+import appData from '../../data/app.json';
+
+const Footer = ({ hideBGCOLOR }) => {
+  return (
+    <footer className={`${!hideBGCOLOR ? 'sub-bg' : ''}`}>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-lg-4'>
+            <div className='item md-mb50'>
+              <div className='title'>
+                <h5>Contact Us</h5>
+              </div>
+              <ul>
+                {/* <li>
+                  <span className='icon pe-7s-map-marker'></span>
+                  <div className='cont'>
+                    <h6>Officeal Address</h6>
+                    <p>504 White St . Dawsonville, GA 30534 , New York</p>
+                  </div>
+                </li> */}
+                <li>
+                  <span className='icon pe-7s-mail'></span>
+                  <div className='cont'>
+                    <h6>Email Me</h6>
+                    <p>rajibhossainprejon@gmail.com</p>
+                  </div>
+                </li>
+                <li>
+                  <span className='icon pe-7s-call'></span>
+                  <div className='cont'>
+                    <h6>Call Me</h6>
+                    <p>+8801518647550</p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className='col-lg-4'>
+            {/* <div className='item md-mb50'>
+                <div className='title'>
+                  <h5>Recent News</h5>
+                </div>
+                <ul>
+                  <li>
+                    <div className='img'>
+                      <Link href='/blog-details/blog-details-dark'>
+                        <a>
+                          <img src='/img/blog/1.jpg' alt='' />
+                        </a>
+                      </Link>
+                    </div>
+                    <div className='sm-post'>
+                      <Link href='/blog-details/blog-details-dark'>
+                        <a>
+                          <p>
+                            The Start-Up Ultimate Guide to Make Your WordPress
+                            Journal.
+                          </p>
+                        </a>
+                      </Link>
+                      <Link href='/blog/blog-dark'>
+                        <a>
+                          <span className='date'>14 sep 2022</span>
+                        </a>
+                      </Link>
+                    </div>
+                  </li>
+                  <li>
+                    <div className='img'>
+                      <Link href='/blog-details/blog-details-dark'>
+                        <a>
+                          <img src='/img/blog/2.jpg' alt='' />
+                        </a>
+                      </Link>
+                    </div>
+                    <div className='sm-post'>
+                      <Link href='/blog-details/blog-details-dark'>
+                        <a>
+                          <p>
+                            The Start-Up Ultimate Guide to Make Your WordPress
+                            Journal.
+                          </p>
+                        </a>
+                      </Link>
+                      <Link href='/blog/blog-dark'>
+                        <a>
+                          <span className='date'>14 sep 2022</span>
+                        </a>
+                      </Link>
+                    </div>
+                  </li>
+                  <li>
+                    <div className='subscribe'>
+                      <input type='text' placeholder='Type Your Email' />
+                      <span className='subs pe-7s-paper-plane'></span>
+                    </div>
+                  </li>
+                </ul>
+              </div> */}
+          </div>
+          <div className='col-lg-4'>
+            <div className='item'>
+              <div className='logo'>
+                <img src={appData.lightLogo} alt='' />
+              </div>
+              <div className='social'>
+                <a
+                  href='https://www.linkedin.com/in/rajib-hossain-prejon-089505202'
+                  target='_blank'
+                >
+                  <i className='fab fa-linkedin mx-4'></i>
+                </a>
+                <a href='https://wa.me/+8801518647550' target='_blank'>
+                  <i className='fab fa-whatsapp mx-4'></i>
+                </a>
+                <a
+                  href='https://github.com/rajib-hossain-prejon/'
+                  target='_blank'
+                >
+                  <i className='fab fa-github mx-4'></i>
+                </a>
+              </div>
+              <div className='copy-right'>
+                <p>
+                  © 2023, Made with passion by
+                  <Link href='/'>
+                    <a>Rajib Hossain Prejon</a>
+                  </Link>
+                  .
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
